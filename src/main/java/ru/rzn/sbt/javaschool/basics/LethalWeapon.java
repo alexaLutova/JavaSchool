333b6cb2785d7a7300f463ea48eac4c3fe699149
alexaLutova <46101511+alexaLutova@users.noreply.github.com>
2019-01-06 14:54:00 +0300

    Add files via upload

--- src/main/java/ru/rzn/sbt/javaschool/basics/LethalWeapon.java
+++ src/main/java/ru/rzn/sbt/javaschool/basics/LethalWeapon.java
@@ -1,10 +1,10 @@
 package ru.rzn.sbt.javaschool.basics;
 
 import java.util.Objects;
-public class LethalWeapon {
 
-    private static long nextSerial = 0;
-    private long serial;
+public class LethalWeapon {
+    public static long nextSerial=0;
+    public final long serial=2;
 
     public long getSerial(){
         return serial;
@@ -17,7 +17,7 @@ public Double getPower(){
   return power;
 }
 
-public void setPower(double power){
+public void setPower(){
     this.power=power;
 }
 
@@ -29,42 +29,35 @@ public void pewPew(){
     roundsLeft-=2;
 }
 
-public LethalWeapon(){
-    serial=nextSerial;
-    nextSerial++;
-}
+public LethalWeapon(){}
 
 
 public LethalWeapon(String color,Double power,int roundsLeft ){
     this.color=color;
     this.power=power;
     this.roundsLeft=roundsLeft;
-    serial=nextSerial;
-    nextSerial++;
+}
+@Override
+public boolean equals(Object obj){
+   if (obj==this) {
+       return true;
+    }
+   if (obj==null) {
+       return false;
+    }
+   if (obj.getClass()!=this.getClass()) {
+       return false;
+    }
+   LethalWeapon k=(LethalWeapon) obj;
+   return power==k.power && roundsLeft==k.roundsLeft &&
+           (color==k.color || (color != null && color.equals(k.color)))            ;
 
 }
 
 @Override
-    public boolean equals(Object obj){
-      if (obj==this) {
-          return true;}
-      if (obj==null){
-          return false;
-      }
-      if (this.getClass() != obj.getClass()) {
-          return false;
-      }
+public int hashCode(){
+       return Objects.hash(color,power,roundsLeft);
+    }
 
-      LethalWeapon k= (LethalWeapon) obj;
-
-      return power==k.power && roundsLeft==k.roundsLeft &&
-              (color==k.color || color != null || color.equals(k.color));
-
-
-}
-@Override
-    public int hashCode(){
-     return Objects.hash(power,roundsLeft,color);
-}
 
 }
