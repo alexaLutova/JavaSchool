66e53b8bc11a6e1b8d8974ba19531302caaadf29
Alexa Lutova <alexa.fiodorova2010@yandex.ru>
2019-01-08 11:26:49 +0300

    Versin

--- java-school-lessons_Vitaliy/src/main/java/ru/rzn/sbt/javaschool/basics/LethalWeapon.java
+++ java-school-lessons_Vitaliy/src/main/java/ru/rzn/sbt/javaschool/basics/LethalWeapon.java
@@ -1,10 +1,10 @@
 package ru.rzn.sbt.javaschool.basics;
 
 import java.util.Objects;
-
 public class LethalWeapon {
-    public static long nextSerial=0;
-    public final long serial=2;
+
+    private static long nextSerial = 0;
+    private long serial;
 
     public long getSerial(){
         return serial;
@@ -17,7 +17,7 @@ public Double getPower(){
   return power;
 }
 
-public void setPower(){
+public void setPower(double power){
     this.power=power;
 }
 
@@ -29,35 +29,41 @@ public void pewPew(){
     roundsLeft-=2;
 }
 
-public LethalWeapon(){}
+public LethalWeapon(){
+    serial=nextSerial;
+    nextSerial++;
+}
 
 
 public LethalWeapon(String color,Double power,int roundsLeft ){
     this.color=color;
     this.power=power;
     this.roundsLeft=roundsLeft;
-}
-@Override
-public boolean equals(Object obj){
-   if (obj==this) {
-       return true;
-    }
-   if (obj==null) {
-       return false;
-    }
-   if (obj.getClass()!=this.getClass()) {
-       return false;
-    }
-   LethalWeapon k=(LethalWeapon) obj;
-   return power==k.power && roundsLeft==k.roundsLeft &&
-           (color==k.color || (color != null && color.equals(k.color)))            ;
+    serial=nextSerial;
+    nextSerial++;
 
 }
 
 @Override
-public int hashCode(){
-       return Objects.hash(color,power,roundsLeft);
-    }
+    public boolean equals(Object obj){
+      if (obj==this) {
+          return true;}
+      if (obj==null){
+          return false;
+      }
+      if (this.getClass() != obj.getClass()) {
+          return false;
+      }
 
+      LethalWeapon k= (LethalWeapon) obj;
+
+      return power==k.power && roundsLeft==k.roundsLeft && (color==k.color || (color != null && color.equals(k.color)));
+
+
+}
+@Override
+    public int hashCode(){
+     return Objects.hash(power,roundsLeft,color);
+}
 
 }
